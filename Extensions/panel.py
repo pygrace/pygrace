15b2683d25b0d1fc81ae839e346ec28a0c89ecc0
deanmalmgren <deanmalmgren@a99734d2-c910-0410-b00f-a7f8a730b8d8>
2009-10-01 16:12:45 +0000

    caught minor bug in panel.

--- Extensions/panel.py
+++ Extensions/panel.py
@@ -261,8 +261,9 @@ Only labels schemes %s are possible.
         """Place all labels in grace at the same time.
         """
         for graph in self.graphs:
-            graph.panel_label.place_label(placement=placement,
-                                          dx=dx,dy=dy,just=just)
+            if isinstance(graph,Panel):
+                    graph.panel_label.place_label(placement=placement,
+                                                  dx=dx,dy=dy,just=just)
 
     def add_graph(self, cls=Panel, *args, **kwargs):
         """Overwrite the add_graph of Grace base so that the default
