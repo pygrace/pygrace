562611c8b1e145c273eb0e2e4badc7004db80dc5
frrmack <frrmack@a99734d2-c910-0410-b00f-a7f8a730b8d8>
2010-02-09 22:07:54 +0000

    Daniel's contribution to directed networks: Elimination of CircleNode, all nodes are grace symbols now (assumed circles) world areas under nodes are calculated accordingly. Also, restructuring, commenting, adapting of the remainder of code and the example around this change, by Irmak

--- Examples/13_directed_network.py
+++ Examples/13_directed_network.py
@@ -10,11 +10,11 @@ from PyGrace.Extensions.network import Network
 # Define a couple of node sets, a xysize set and a regular xy set
 # The sizes will be the radii of the nodes in world coordinates.
 nodeset1_xysize = {
-    'a1' : (0.1, 0.1, 0.03),
-    'a2' : (0.8, 0.1, 0.02),
-    'a3' : (0.1, 0.8, 0.04),
-    'a4' : (0.8, 0.8, 0.06),
-    'a5' : (0.865, 0.865, 0.02),
+    'a1' : (0.1, 0.1, 3),
+    'a2' : (0.8, 0.1, 2),
+    'a3' : (0.1, 0.8, 4),
+    'a4' : (0.8, 0.8, 6),
+    'a5' : (0.865, 0.865, 2),
     }
 nodeset2_xy = {
     'B' : (0.5, 0.5),
@@ -52,61 +52,86 @@ grace = Grace()
 # Create the network graph instance
 network = grace.add_graph(Network)
 
-# Up to here, the setup is the same as undirected networks
+# Make sure the view and the world are squares
+# This is *essential* since the link drawing code relies 
+# upon the world and view being defined BEFORE their instantiation.
+#         (You want to work with squares in any case, because
+#          otherwise coordinates shift, distances are misperceived)
+network.set_view(0,0,1,1)
+network.set_world(0,0,1,1)
 
-# ------------
+
+# ----------------------------
 # Add the node sets
- 
-# To use link options like avoiding going under nodes, or having
-# arrowheads appear right where the target node starts, the nodes
-# need to be CircleNodes, and those are added one by one.
-for id, data in nodeset1_xysize.items():
-    network.add_circle_node(id, data, color=3, outline_color=1)
+# ----
 
+# nodeset 1 ----------
+network.add_node_set(nodeset1_xysize, type = 'xysize',
+                     color=3, line_width=1, line_color=1)
+
+
+# nodeset 2 ----------
 for id, (x,y) in nodeset2_xy.items():
     # you don't need to define a size, but I'll make them big
     # to demonstrate curving of links to avoid node obstacles
-    bigradius = .08
+    # This also demonstrates add_node (it adds a single node)
+    bigradius = 8
     data = (x,y, bigradius)
-    network.add_circle_node(id, data, color=11, line_width=6, line_color=12)
+    network.add_node(id, data, type='xysize', color=11,
+                     line_width=6, line_color=11)
 
-# You don't have to define a size, CircleNode can attach a
+
+# nodeset 3 ----------
+# You don't have to define a size, nodes will have a
 # default size if your data is of type xy only.
 for id, data in nodeset3_xy.items():
-    network.add_circle_node(id, data, color=1)
+    network.add_node(id, data)
+# Note that you don't need the for loop, this also works:
+# network.add_node_set(nodeset3_xy)
 
-# This still works, but cannot use mentioned options 
+
+# nodeset 4 ----------
+# If you're adding a nodeset of type xy, you don't have to
+# specifically indicate that 
 network.add_node_set(nodeset4_xy, size=2, color=13, line_width=6, line_color=12)
-# -------
 
-# ------------
+# ---------------------------------
+
+# ----------------------------------
 # Add the link sets
+# ------
 
 # For directed link sets, layout options and their default values are:
 # avoid_crossing_nodes = True  (curve around -if you can- other nodes.
 #                               also, if target and source are close and
 #                               big, so that almost all the link is under
 #                               them, try to curve away to show yourself)
-# put_arrows = True            (put arrows -Drawing objects-)
+# put_arrows = True            (put arrowheads -Drawing objects-)
 # arrow_position = 0.75        (where to put the arrow. 1.0 means at the 
 #                               farthest visible point, 0.0 means at
 #                               nearest visible point from the origin.)
-# curvature = .6               (how much each link curves by default)  
+# curvature = .6               (how much each link curves by default. 
+#                               positive curvature is to the right.)  
+
+# links 1 ---------------
 dirlinkSet = network.add_directed_link_set(links1, size=0.5,
                                            color=2,
                                            curvature = .2,
                                            arrow_position = 1.0)
 dirlinkSet.line.linestyle = 3  # dashed links
 
+# links 2 ----------------
 # these links include links between a4 and a5, these will automatically
 # curve more to show themselves (part of avoid_crossing_nodes)
 network.add_directed_link_set(links2, size=5, color=9)
 
-# Can still add a single node and a single undirected link
+# single undirected ------
+# You can still add a single node and a single undirected link ...
 network.add_node('alone', (.3, .4, 5), type='xycolor')
 network.add_link(('alone', 'B'))
 
-# or a directed one
+# single directed --------
+# ... or a directed one
 network.add_directed_link(('d', 'e'), curvature= -1.5,
                           size=3, color = 6,
                           put_arrows = False)
@@ -115,12 +140,7 @@ network.add_directed_link(('k', 'l'), curvature=0,
                           size=3, color = 3,
                           arrow_position=0.5)
 
-
-# ------
-
-# Make sure the view and the world are squares
-network.set_view(0,0,1,1)
-network.set_world(0,0,1,1)
+# ------------------------------------
 
 # Print out the grace
 grace.write_file('13_directed_network.agr')
--- Extensions/network.py
+++ Extensions/network.py
@@ -4,7 +4,6 @@ from PyGrace.grace import Grace
 from PyGrace.graph import Graph
 from PyGrace.dataset import DataSet
 from PyGrace.drawing_objects import DrawLine, DrawText
-from datastat import frange
 from math import sqrt, sin, cos
 
 class NodeSet(DataSet):
@@ -52,97 +51,6 @@ class NodeSet(DataSet):
         self.line.linestyle = 0
         if labels:
             self.avalue.onoff='on'
-
-
-class SolidCircle(DataSet):
-    """A dataset that shows up as a solid circle.
-
-    data for the creating of the SolidCircle must be the
-    center of the circle and a radius.
-    """
-    def __init__(self, color=1, outline_color = None, *args, **kwargs):
-        DataSet.__init__(self, *args, **kwargs)
-        if len(self.data) != 3 and len(self.data.values()[0]) != 3:
-            raise TypeError, 'Data for SolidCircle MUST contain 3 items (x,y & radius)'
-        if type(self.data) in (tuple, list):
-            # data is (x,y,r)
-            x0 = self.x0 = self.data[0]
-            y0 = self.y0 = self.data[1]
-            r  = self.r  = self.data[2]
-        elif type(self.data) == dict:
-            # data is {label: (x,y,r)}
-            val = self.data.values()[0]
-            x0 = self.x0 = val[0]
-            y0 = self.y0 = val[1]
-            r  = self.r  = val[2]
-            self.labeleddata = {}
-            for key,val in self.data.items():
-                self.labeleddata[key] = val
-        def circle(t):
-            x = x0 + r*cos(t)
-            y = y0 + r*sin(t)
-            return x,y
-        self.data = [circle(t) for t in frange(0, 6.3, .1)]
-        self.symbol.configure(shape=0)
-        if outline_color is None:
-            self.line.configure(linewidth=0, color=color)
-        else:
-            self.line.configure(linewidth=2, color=outline_color)
-        self.fill.configure(type=1, rule=0, color=color)
-
-
-
-class CircleNode(SolidCircle):
-    """ A special nodeset for one node, using a SolidCircle dataset to
-    represent the node.
-
-    The advantage of CircleNode over a normal nodeset is that the area
-    occupied by the node is set in world coordinates. A symbol keeps
-    its size and shape when zoomed in or when axis limits are
-    changed. This is good in many cases, but it makes it very
-    difficult to find out if something is in the area the node
-    covers. CircleNode has the method in_circle, that finds out if
-    that's the case. The disadvantage of using world coordinates is
-    that if the x- and y- sides of the View are scaled differently
-    (which is default behavior in grace), the circle turns into an
-    ellipse. They have to be scaled in the same way, i.e. xmin=ymin,
-    xmax=ymax for view coordinates. In other words, the View must be a
-    square.  This is usually the case when working with coordinates
-    anyway, to avoid disproportions in placements of items.
-    """
-    def __init__(self, color=2, outline_color=None, labels=False, *args, **kwargs):
-        data = kwargs['data']
-        for key in data.keys():
-            if len(data[key]) == 2:
-                xmin,ymin,xmax,ymax = kwargs['parent'].get_world()
-                default_size = 0.03 * abs(xmax - xmin)
-                x,y = data[key]
-                data[key] = (x , y, default_size)
-        SolidCircle.__init__(self, color=color, outline_color=outline_color,
-                             *args, **kwargs)
-        # self.data is changed at this moment. It is a list of points
-        # defining the circle.
-        for aNode in self.labeleddata:
-            # to reach all the beautiful methods of CircleNode (in_circle),
-            # we want to keep the whole node instance in the memory as well :)
-            self.parent.nodes[aNode] = self
-            # and now the regular xy and size stuff as well
-            self.parent.node_xy[aNode] = (self.x0, self.y0)
-            if aNode not in self.parent.node_sz:
-                self.parent.node_sz[aNode] = self.r      
-        if labels:
-            self.avalue.onoff='on'
-        
-    def in_circle(self,x,y):
-        x0 = self.x0
-        y0 = self.y0
-        r  = self.r
-        if (x-x0)**2+(y-y0)**2 < r**2:
-            return True
-        return False
-
-
-
 
 class LinkSet(DataSet):
     """A dataset containing network links.
@@ -255,6 +163,21 @@ class DirectedLinkSet(LinkSet):
                  curvature = .6,
                  arrow_position = 0.75,
                  *args, **kwargs):
+        
+            # -- Preferences (Fixed manual tweaks - not arguments) -----
+
+            # radius buffer percentage for determining where the circle
+            # of a node ends. To avoid links curving around but still
+            # touching the nodes
+            node_border_buffer_percentage = 0.08
+
+            # The threshold percentage of points in a link that are under the target
+            # and the source. If this much or more are covered, the links try to bend
+            # themselves, so that more of them are visible.
+            bad_cover_percentage = 0.29
+
+            # ----------------------
+
             DataSet.__init__(self, *args, **kwargs)
             if arrow_position > 1.0:
                 arrow_position = 1.0
@@ -263,39 +186,59 @@ class DirectedLinkSet(LinkSet):
 
             newData = []
 
+            # -- loop over each link -- #
             for n1, n2, in self.data:
                 curveData = []
 
                 xi, yi = self.parent.node_xy[n1]
                 xf, yf = self.parent.node_xy[n2]
-                try:
-                    source = self.parent.nodes[n1]
-                    target = self.parent.nodes[n2]
-                except KeyError:
-                    # nodes are not stored in self.parent.nodes
-                    # the following option won't work, turn them off
-                    avoid_crossing_nodes = False
-                    source, target = None, None
+
+                source = n1
+                target = n2
 
                 # Quadratic Bezier curve
                 curve = Bezier(xi,yi,xf,yf, curvature=curvature)
 
-                #######################################################
-                # Now check in the curve is under any other node      #
-                # but the source and the target. You can only do      #
-                # this if the nodes are CircleNode, of course.
-                # If they are, change curvature, check again.
+                wxmin,wymin,wxmax,wymax = kwargs['parent'].get_world()
+                vxmin,vymin,vxmax,vymax = kwargs['parent'].get_view()
+
+                # compute the scaling factor between items in the world space and in the view space
+                # asuming that one uses circular node symbols
+                # if the view space is not square, there are potential issues
+                ratio = min((wxmax - wxmin)/(vxmax - vxmin),(wymax - wymin)/(vymax - vymin))
+
+                def point_under_node(x,y,node, buffer_perc=0.00):
+                    """given a point and a node label,
+                    checks if point is under the node symbol"""
+                    # get node info
+                    x0, y0 = self.parent.node_xy[node]
+                    r = (self.parent.node_sz[node]/100.) * ratio * (1 + buffer_perc)
+                    # check if point is under the node (within the circle)
+                    if (x-x0)**2+(y-y0)**2 <= r**2:
+                        return True
+                    else:
+                        return False
+
+
                 if avoid_crossing_nodes:
+                ####################################################################
+                # Now check if the curve is under any other node                   #
+                # but the source and the target. If it is, change curvature,       #
+                # check again:                                                     #
+                # CURVE AROUND NODES IN YOUR WAY
+                #
                     while True:
                         giveup = False
                         recheck = False
-                        increment = .3
+                        increment = 0.3
                         for x,y in curve.points(1000)[1:-1]:
-                            for n in self.parent.nodes:
-                                node = self.parent.nodes[n]
-                                if not hasattr(node, 'in_circle') or n in (n1, n2):
+                            for n in self.parent.node_xy:
+                                if n in (source, target):
+                                    # we won't try to curve around these 
                                     continue
-                                if node.in_circle(x,y):
+                                # check if point is under the node (within the circle)
+                                if point_under_node(x,y,n, buffer_perc = node_border_buffer_percentage):
+                                    # point is indeed under it. curve away and recheck.
                                     recheck = True
                                     if 0 < curve.curvature < 10:
                                         curve.change_curvature(curve.curvature + increment)
@@ -315,17 +258,27 @@ class DirectedLinkSet(LinkSet):
                         else:
                             #end of cheking without breaks. Passed the test.
                             break # the while loop
-                        
-                    # Check if all the curve is under source and target nodes
-                    # If so, curve it away and back in (give it a high curvature)
+                #                                                                  #
+                #                                                                  #
+                ######------------                                    -------#######
+                #     Check if all the curve is under source and target nodes      #
+                #     If so, curve it away and back in (give it a high curvature): #
+                #     FLARE OUT IF SOURCE AND TARGET COVER YOU
+                #
                     while True:
                         covered = 0
                         for x,y in curve.points(40)[1:-1]:
-                            for node in (source, target):
-                                if not hasattr(node, 'in_circle'): break
-                                if node.in_circle(x,y):
+                            for n in (source, target):
+                                # check if point (x,y) is in the node
+                                if point_under_node(x,y,n):
                                     covered += 1
-                        if covered/(2*38.) > 0.29:
+                        if covered/(2*38.) >= bad_cover_percentage:
+                            # bad_cover_percentage or more is covered
+                            # bend a little to show more of yourself
+                            # do this again and again until either
+                            # the coverage is less than the threshold
+                            # or you curved a lot but still couldn't get away
+                            # ( threshold to give up: |curvature| = 15  )
                             if 0 < curve.curvature <= 5:
                                 curve.change_curvature(curve.curvature + .4)
                             elif curve.curvature > 5:
@@ -335,9 +288,10 @@ class DirectedLinkSet(LinkSet):
                             elif curve.curvature < -15:
                                 break # give up
                             #print n1,n2, 'cover %.1f %%' % (100*covered/(2*38.)), ' -> new curvature', curve.curvature
-                        else:                                        #
-                            break                                    #
-                ######################################################
+                        else:                                                    #
+                            break                                                #
+                #                                                                #
+                ##################################################################
 
                 # This is a linkset, so every point except the
                 # first and the last ones must appear twice
@@ -348,7 +302,12 @@ class DirectedLinkSet(LinkSet):
                     curveData.append((x, y))
                 curveData.append((xf, yf))
 
+
                 if put_arrows:
+                ##################################################################
+                # PUT AN ARROWHEAD ON THE LINK                                   #
+                #                                                                
+                #
                     # Find the position of the arrow
                     # the range of visible points
                     def visible_points(node1, node2):
@@ -356,23 +315,14 @@ class DirectedLinkSet(LinkSet):
                         # center of second node, going back, stop when
                         # you found the fist point NOT under the node.
                         # (the edge of it). node1 and node2 are instances
-                        # of start & end nodes. If these are not CircleNode,
-                        # the position is just the end.
+                        # of start & end nodes. 
                         domain = []
-                        # If not CircleNodes, assume everything's visible
-                        if (not hasattr(node1, 'in_circle')) or \
-                                (not hasattr(node2, 'in_circle')):
-                            domain.append(curveData[0])
-                            for i in range (1, len(curveData)-1, 2):
-                                domain.append(curveData[i])
-                            domain.append(curveData[-1])
-                            return domain
                         # Otherwise, scan for when we get out
                         # from under node 1:
                         node_1_out = len(curveData)-2
                         for i in range(1, len(curveData)-1, 2):
                             x,y = curveData[i]
-                            if not node1.in_circle(x,y):
+                            if not point_under_node(x,y,node1):
                                 # We broke out from under node 1.
                                 # Check if we are already under node 2.
                                 node_1_out = i
@@ -380,11 +330,11 @@ class DirectedLinkSet(LinkSet):
                         # If we are already under node 2, nothing is
                         # visible go backwards, say the visible
                         # part is the intersection of nodes!
-                        if node2.in_circle(x,y):
+                        if point_under_node(x,y,node2):
                             for i in range(node_1_out, 1, -2):
                                 domain.insert(0, curveData[i])
                                 x,y = curveData[i]
-                                if not node2.in_circle(x,y):
+                                if not point_under_node(x,y,node2):
                                     if len(domain) == 1: domain.insert(0, curveData[i-1])
                                     return domain
                             # end of for, we are back at start
@@ -393,9 +343,9 @@ class DirectedLinkSet(LinkSet):
                         node_2_out = len(curveData)-2
                         for i in range(node_1_out, len(curveData)-1, 2):
                             x,y = curveData[i]
-                            if node2.in_circle(x,y):
+                            if point_under_node(x,y,node2):
                                 node_2_out = i
-                                if len(domain) == 1: domain.append(cureveData[i+1])
+                                if len(domain) == 1: domain.append(0, curveData[i+1])
                                 return domain
                             else:
                                 domain.append(curveData[i])
@@ -411,7 +361,6 @@ class DirectedLinkSet(LinkSet):
 
                     # Find your arrow position
                     domain = visible_points(source, target)
-                    domain.reverse()
                     i = int(arrow_position * len(domain)) -1
                     if i < 1: i = 1
                     start, end = domain[i-1], domain[i]
@@ -427,7 +376,11 @@ class DirectedLinkSet(LinkSet):
                                                    arrow_type=1,
                                                    arrow_length = 1.5,
                                                    arrow_layout=(0.8,0.8))
-                    ###############
+                #                                                                #
+                #                                                                #
+                ##################################################################
+
+                      #---------------#
 #                     #Write the i j of links (for debugging)
 #                     self.parent.add_drawing_object(DrawText,
 #                                                    char_size = .8,
@@ -437,10 +390,12 @@ class DirectedLinkSet(LinkSet):
 #                                                    x = start[0],
 #                                                    y=start[1],
 #                                                    color=color)
-                    #################
+                      #---------------#
 
                 # put the calculated link into the collective data for the linkset.
                 newData.extend(curveData)
+
+            # -- end of loop over each link -- #
 
             # Configure the whole linkset
             self.data = newData
@@ -506,20 +461,6 @@ class Network(Graph):
                                 NodeSet,
                                 size=size, color=color,
                                 line_width=line_width, line_color=line_color,
-                                *args, **kwargs)
-
-    def add_circle_node(self, label, node_data,
-                                  *args, **kwargs):
-        """Add a single circle node to the graph.
-
-        The node will be added to the Network graph as a CircleNode.
-
-        -label contains the label of the node.
-
-        -node_data needs to be the (x,y,r) coordinates of the node and the radius (size)
-        """
-        return self.add_dataset({label: node_data},
-                                CircleNode,
                                 *args, **kwargs)
 
     def add_link_set(self, node_pairs, size=1, color=1, *args, **kwargs):
